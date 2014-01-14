fda6d544405fd291c42989d6b52ddb0189d6700b
Guillaume <guillaume.surget@isep.fr>
2014-01-14 10:52:12 +0100

    Projet Java Final Surget DO VAN

--- Cars/src/carsEditorCircuit/EditorBoard.java
+++ Cars/src/carsEditorCircuit/EditorBoard.java
@@ -24,27 +24,39 @@ public class EditorBoard extends JPanel implements MouseListener, MouseMotionLis
 	//MouseWheelListener : lectured de la molette de la souris
 	//KeyListener : lecture du clavier
 	
-	String editeur[][] = new String[20][20]; // Tableau gérant notre panel de 20*20 images
-	String imageSelect[] = {"WALL", "ROAD", "GRASS"}; //Tableau contenant les images 
-	String imageCourante = "WALL"; // Image selectionnée
-	int mX, mY; //variable pour la position de la souris
+	String editeur[][] = new String[30][25]; // Tableau gérant notre panel de 20*20 images
+	String imageSelect[] = {"MUR", "ROUTE", "HERBE", "EAU", "SABLE", "ARRIVEE" }; //Tableau contenant les images 
+	String imageCourante = "MUR"; // Image selectionnée
+	int sourisX, sourisY; //variable pour la position de la souris
 	int index = 0; //index de l'image courante
-	Image wall;//wall est du type image 
-	Image road;
-	Image grass;
+	Image mur;//wall est du type image 
+	Image route;
+	Image herbe;
+	Image eau;
+	Image sable;
+	Image arrivee;
 	FileWriter fw;
 	FileReader fr;
 	
 	// Constructeur //
 	public EditorBoard(){
-		ImageIcon imageGrass = new ImageIcon("Images/grass.jpg"); //importation de l'image "grass" dans la variable imageGrass
-		grass = imageGrass.getImage();//copie de l'image contenue dans imageGrass vers la variable Grass
+		ImageIcon imageMur = new ImageIcon("Images/Textures/lego_blanc.png"); //importation de l'image "grass" dans la variable imageGrass
+		mur = imageMur.getImage();//copie de l'image contenue dans imageGrass vers la variable Grass
 		
-		ImageIcon imageRoad = new ImageIcon("Images/road.jpg");
-		road = imageRoad.getImage();
+		ImageIcon imageRoute = new ImageIcon("Images/Textures/asphalt.jpg");
+		route = imageRoute.getImage();
 		
-		ImageIcon imageWall = new ImageIcon("Images/wall.jpg");
-		wall = imageWall.getImage();
+		ImageIcon imageHerbe = new ImageIcon("Images/Textures/lego_vert.png");
+		herbe = imageHerbe.getImage();
+		
+		ImageIcon imageEau = new ImageIcon("Images/Textures/lego_bleu.png");
+		eau = imageEau.getImage();
+		
+		ImageIcon imageSable = new ImageIcon("Images/Textures/lego_jaune.png");
+		sable = imageSable.getImage();
+		
+		ImageIcon imageArrivee = new ImageIcon("Images/Textures/lego_rouge.png");
+		arrivee = imageArrivee.getImage();
 		
 		setFocusable(true);
 		
@@ -60,30 +72,47 @@ public class EditorBoard extends JPanel implements MouseListener, MouseMotionLis
 		super.paint(g); //appelle la méthode paint de la classe parente de EditorBoad : JPanel
 		Graphics2D g2d = (Graphics2D) g; //on caste g en Graphics2D qui offre plus de possibilités
 		
-		for(int i=0; i<19; i++){
-			for(int j=0; j<19; j++){//deplacement dans le tableau de notre panel
-				if(editeur[i][j] == "WALL"){
-					g2d.drawImage(wall, i*30, j*30, null);//si l'editeur contient l'image WALL alors on dessine wall à l'emplacement i*30 et j*30 car l'image fait 30*30pixels
-				}
+		for(int i=0; i<30; i++){
+			for(int j=0; j<25; j++){//deplacement dans le tableau de notre panel
 				
-				if(editeur[i][j] == "ROAD"){
-					g2d.drawImage(road, i*30, j*30, null);
+				if(editeur[i][j] == "MUR"){
+					g2d.drawImage(mur, i*30, j*30, null);//si l'editeur contient l'image MUR alors on dessine mur à l'emplacement i*30 et j*30 car l'image fait 30*30pixels
 				}
-				
-				if(editeur[i][j] == "GRASS"){
-					g2d.drawImage(grass, i*30, j*30, null);
+				if(editeur[i][j] == "ROUTE"){
+					g2d.drawImage(route, i*30, j*30, null);
+				}
+				if(editeur[i][j] == "HERBE"){
+					g2d.drawImage(herbe, i*30, j*30, null);
+				}
+				if(editeur[i][j] == "EAU"){
+					g2d.drawImage(eau, i*30, j*30, null);
+				}
+				if(editeur[i][j] == "SABLE"){
+					g2d.drawImage(sable, i*30, j*30, null);
+				}
+				if(editeur[i][j] == "ARRIVEE"){
+					g2d.drawImage(arrivee, i*30, j*30, null);
 				}
 			}
 		}
 		
-		if(imageCourante == "WALL"){
-			g2d.drawImage(wall, mX, mY, null);//dessine l'image courante à la position de la souris
+		if(imageCourante == "MUR"){
+			g2d.drawImage(mur, sourisX, sourisY, null);//dessine l'image courante à la position de la souris
 		}
-		else if(imageCourante == "ROAD"){
-			g2d.drawImage(road, mX, mY, null);
+		else if(imageCourante == "ROUTE"){
+			g2d.drawImage(route, sourisX, sourisY, null);
 		}
-		else if(imageCourante == "GRASS"){
-			g2d.drawImage(grass, mX, mY, null);
+		else if(imageCourante == "HERBE"){
+			g2d.drawImage(herbe, sourisX, sourisY, null);
+		}
+		else if(imageCourante == "EAU"){
+			g2d.drawImage(eau, sourisX, sourisY, null);
+		}
+		else if(imageCourante == "SABLE"){
+			g2d.drawImage(sable, sourisX, sourisY, null);
+		}
+		else if(imageCourante == "ARRIVEE"){
+			g2d.drawImage(arrivee, sourisX, sourisY, null);
 		}
 	}
 	@Override
@@ -99,18 +128,27 @@ public class EditorBoard extends JPanel implements MouseListener, MouseMotionLis
 		if(key == 's'){
 			try{
 				fw = new FileWriter (JOptionPane.showInputDialog(null, "Entrez le chemin de sauvegarde :", "EDITEUR DE CIRCUITS", JOptionPane.QUESTION_MESSAGE));//Ouvre une fenetre de dialogue pour entrez le chemin d'enregistrement du fichier
-				for (int i=0; i<20; i++ ){
-					for (int j=0; j<20; j++){// on parcourt le tableau editeur
-						if(editeur[j][i] == "WALL"){
-							fw.write('w');//on écrit dans le fichier un "0" si il y a un "wall" dans l'editeur
+				for (int j=0; j<25; j++ ){
+					for (int i=0; i<30; i++){// on parcourt le tableau editeur
+						if(editeur[i][j] == "MUR"){
+							fw.write('m');//on écrit dans le fichier un "m" si il y a un "wall" dans l'editeur
 						}
-						else if(editeur[j][i] == "ROAD"){
+						else if(editeur[i][j] == "ROUTE"){
 							fw.write('r');
 						}
-						else if(editeur[j][i] == "GRASS"){
-							fw.write('g');
+						else if(editeur[i][j] == "HERBE"){
+							fw.write('h');
 						}
-						else if(editeur[j][i] == null){
+						else if(editeur[i][j] == "EAU"){
+							fw.write('e');
+						}
+						else if(editeur[i][j] == "SABLE"){
+							fw.write('s');
+						}
+						else if(editeur[i][j] == "ARRIVEE"){
+							fw.write('a');
+						}
+						else if(editeur[i][j] == null){
 							fw.write(' '); //on écrit un espace si il n'y a rien dans l'éditeur
 						}
 					}
@@ -129,24 +167,33 @@ public class EditorBoard extends JPanel implements MouseListener, MouseMotionLis
 				while((i = fr.read()) != -1){//on continue à lire tant qu'on a pas atteint -1, signifiant la fin de la lecture
 					char strImg = (char) i; //on caste i en chaine de caractère pour pouvoir lire le fichier
 					
-					if(strImg == 'w'){ 
-						editeur[x][y] = "WALL";//si strImg vaut 'w' alors on met un "WALL" dans l'éditeur
+					if(strImg == 'm'){ 
+						editeur[x][y] = "MUR";//si strImg vaut 'm' alors on met un "MUR" dans l'éditeur
 					}
 					else if(strImg == 'r'){
-						editeur[x][y] = "ROAD";
+						editeur[x][y] = "ROUTE";
 					}
-					else if(strImg == 'g'){
-						editeur[x][y] = "GRASS";
+					else if(strImg == 'h'){
+						editeur[x][y] = "HERBE";
+					}
+					else if(strImg == 'e'){
+						editeur[x][y] = "EAU";
+					}
+					else if(strImg == 's'){
+						editeur[x][y] = "SABLE";
+					}
+					else if(strImg == 'a'){
+						editeur[x][y] = "ARRIVEE";
 					}
 					else if(strImg == ' '){
 						editeur[x][y] = null;
 					}
-					else if(strImg == '\n' || strImg == '\r'){//si on arrive à la fin de la ligne on décrémente x afin de revenir à x=19
+					else if(strImg == '\n' || strImg == '\r'){//si on arrive à la fin de la ligne on décrémente x afin de revenir à x=29
 						x--;//
 					}
 					
-					if(x == 19){
-						y++;//si x=19 on avance d'une ligne et on remet x à 0
+					if(x == 29){
+						y++;//si x=29 on avance d'une ligne et on remet x à 0
 						x =0;
 					}
 					else{
@@ -175,7 +222,7 @@ public class EditorBoard extends JPanel implements MouseListener, MouseMotionLis
 			}
 		}
 		else if(rotation > 0){
-			if(index < 2){//rotation positive => on incrémente l'index qui reste bloqué sur l'image si supérieur  
+			if(index < 5){//rotation positive => on incrémente l'index qui reste bloqué sur l'image si supérieur  
 				index++;  
 			}
 		}
@@ -191,8 +238,8 @@ public class EditorBoard extends JPanel implements MouseListener, MouseMotionLis
 
 	@Override
 	public void mouseMoved(MouseEvent arg0) {
-		mX = arg0.getX()-15;//la variable mX prend la coordonnée x de la position de la souris à laquelle on soustrait 15 pour que l'image soit centrée 
-		mY = arg0.getY()-15;
+		sourisX = arg0.getX()-15;//la variable mX prend la coordonnée x de la position de la souris à laquelle on soustrait 15 pour que l'image soit centrée 
+		sourisY = arg0.getY()-15;
 		
 		repaint(); //appelle la méthode paint
 	}
@@ -200,25 +247,20 @@ public class EditorBoard extends JPanel implements MouseListener, MouseMotionLis
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
 	}
-
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -234,6 +276,5 @@ public class EditorBoard extends JPanel implements MouseListener, MouseMotionLis
 		}
 		
 		repaint();
-		
 	}
 }
--- Cars/src/carsEditorCircuit/EditorFrame.java
+++ Cars/src/carsEditorCircuit/EditorFrame.java
@@ -8,8 +8,8 @@ public class EditorFrame extends JFrame {
 	public EditorFrame(){
 		this.setTitle("Circuit Editor !"); // Titre de notre fenêtre
 		//this.setLocation(null);// Position de notre fenêtre, null=>centrée dans l'écran
-		this.setSize(600,600); // Taille de notre fenetre, 30 images de 20 pixels => 600
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Ferme la fenetre et arrete le programme en cliquant sur la croix
+		this.setSize(905,780); // Taille de notre fenetre, 30 images*25 images de 30 pixels => 900 et 750 on ajoute à cela les bodures supérieure infèrieure et latérales
+		//this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Ferme la fenetre et arrete le programme en cliquant sur la croix
 		this.add(new EditorBoard());// Ajoute à la fenetre EditorBoard comme content pane
 		this.setResizable(false);//Redimensionnement de la fenetre desactivé
 		this.setVisible(true);//rend la fenetre et son contenu visible
